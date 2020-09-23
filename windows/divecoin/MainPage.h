1196bb6a0b444a9ef425efe726a5fac38fad0545
LightSpeed <richardcareaga@gmail.com>
2020-09-23 09:10:19 -0700

    V1.0
    Just Started Project

---
+++ windows/divecoin/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::divecoin::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::divecoin::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
